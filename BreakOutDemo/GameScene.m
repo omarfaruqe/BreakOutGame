218765c5c5b62ccdbed8a173699b3109a1feefbd
Omar Faruqe <faruqe.cse@gmail.com>
2016-04-01 01:30:56 -0400

    ball Joint

--- BreakOutDemo/GameScene.m
+++ BreakOutDemo/GameScene.m
@@ -12,6 +12,43 @@
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
+    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    
+    SKSpriteNode *ball1 = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+    ball1.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball1.size.width / 2];
+    ball1.physicsBody.dynamic = YES;
+    ball1.position = CGPointMake(100, self.size.height / 2);
+    ball1.physicsBody.friction = 0.0;
+    ball1.physicsBody.restitution = 1.0;
+    ball1.physicsBody.linearDamping = 0.0;
+    ball1.physicsBody.angularDamping = 0.0;
+    ball1.physicsBody.allowsRotation = NO;
+    ball1.physicsBody.mass = 1.0;
+    ball1.physicsBody.velocity = CGVectorMake(200.0, 200.0);
+    
+    
+    SKSpriteNode *ball2 = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+    ball2.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball2.size.width / 2];
+    ball2.physicsBody.dynamic = YES;
+    ball2.position = CGPointMake(300, self.size.height / 2);
+    ball2.physicsBody.friction = 0.0;
+    ball2.physicsBody.restitution = 1.0;
+    ball2.physicsBody.linearDamping = 0.0;
+    ball2.physicsBody.angularDamping = 0.0;
+    ball2.physicsBody.allowsRotation = NO;
+    ball2.physicsBody.mass = 1.0;
+    ball2.physicsBody.velocity = CGVectorMake(0.0, 220.0);
+    
+    [self addChild:ball1];
+    [self addChild:ball2];
+    
+    CGPoint ball1Anchor = CGPointMake(ball1.position.x, ball1.position.y);
+    CGPoint ball2Anchor = CGPointMake(ball2.position.x, ball2.position.y);
+    
+    SKPhysicsJointSpring *joint = [SKPhysicsJointSpring jointWithBodyA:ball1.physicsBody bodyB:ball2.physicsBody anchorA:ball1Anchor anchorB:ball2Anchor];
+    joint.damping = 0.0;
+    joint.frequency = 1.5;
+    [self.scene.physicsWorld addJoint:joint];
 }
 
 
