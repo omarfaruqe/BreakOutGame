80a2638226ed917b6e350359e83d1fedda99849c
Omar Faruqe <faruqe.cse@gmail.com>
2016-03-31 21:47:48 -0400

    Initial Commit

---
+++ BreakOutDemo/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  BreakOutDemo
+//
+
+//  Copyright (c) 2016 Omar Faruqe. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
